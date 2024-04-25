7a4fdd14eab5c67612ccd16a724f19bc6f86eb9f
FATAYERJI Ahmad <ahmad.fatayerji@etu.univ-nantes.fr>
2024-04-25 22:45:49 +0200

    added produit_scalaire method

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ vector<double> addition(vector<double> v1, vector<double> v2){
 
     //vérfication de la taille des vecteurs
     if(v1.size() != v2.size()){
-        throw invalid_argument("Les vecteurs ne sont pas de meme taille");
+        throw invalid_argument("Les vecteurs ne sont pas de même taille");
     }
 
     //addition des vecteurs
@@ -36,7 +36,7 @@ vector<double> multiplication_scalaire(vector<double> v, double scalar) {
     vector<double> result(v.size());
 
     // Multiplication de chaque élément du vecteur par le scalaire
-    for (int i = 0; i < v.size(); i++) {
+    for (size_t i = 0; i < v.size(); i++) {
         result[i] = v[i] * scalar;
     }
 
@@ -45,13 +45,24 @@ vector<double> multiplication_scalaire(vector<double> v, double scalar) {
 
 //******************************************************************************************************************************
 
- vector<double> produit_scalaire(vector<double> ){
+ vector<double> produit_scalaire(vector<double> v1, vector<double> v2){
+    
+    //variables
+    vector<double> result(v1.size());
 
+    //vérfication de la taille des vecteurs
+    if(v1.size() != v2.size()){
+        throw invalid_argument("Les vecteurs ne sont pas de même taille");
+    }
+
+    //scalar product of the vectors
+    for (size_t i = 0; i < v1.size(); i++)
+    {
+        result[i]= v1[i] * v2[i];
+    }
+    
+    return result;
  }
-
-
-
-
 
 //******************************************************************************************************************************
 int main(){
