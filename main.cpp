6dfaeb216b637b6d74bb70c1cf47d5457c48ea90
slowinski <slowinski.konstantine@gmail.com>
2024-05-01 17:50:12 +0200

    changed doubles to long douubles

--- main.cpp
+++ main.cpp
@@ -12,10 +12,10 @@ using namespace std;
 
 //******************************************************************************************************************************
 
-vector<double> addition(vector<double> v1, vector<double> v2){
+vector<long double> addition(vector<long double> v1, vector<long double> v2){
 
     //variables
-    vector<double> result(v1.size());
+    vector<long double> result(v1.size());
 
     //vérfication de la taille des vecteurs
     if(v1.size() != v2.size()){
@@ -33,10 +33,10 @@ vector<double> addition(vector<double> v1, vector<double> v2){
 
 //******************************************************************************************************************************
 
-vector<double> multiplication_scalaire(vector<double> v, double scalar) {
+vector<long double> multiplication_scalaire(vector<long double> v, long double scalar) {
     
     //variables
-    vector<double> result(v.size());
+    vector<long double> result(v.size());
 
     // Multiplication de chaque élément du vecteur par le scalaire
     for (size_t i = 0; i < v.size(); i++) {
@@ -48,10 +48,10 @@ vector<double> multiplication_scalaire(vector<double> v, double scalar) {
 
 //******************************************************************************************************************************
 
- vector<double> produit_scalaire(vector<double> v1, vector<double> v2){
+ vector<long double> produit_scalaire(vector<long double> v1, vector<long double> v2){
     
     //variables
-    vector<double> result(v1.size());
+    vector<long double> result(v1.size());
 
     //vérfication de la taille des vecteurs
     if(v1.size() != v2.size()){
@@ -70,11 +70,11 @@ vector<double> multiplication_scalaire(vector<double> v, double scalar) {
 //******************************************************************************************************************************
 // Fonction pour transposer une matrice
 
-vector<vector<double>> transpose_matrice(const vector<vector<double>>& matrix) {
+vector<vector<long double>> transpose_matrice(const vector<vector<long double>>& matrix) {
     if (matrix.empty()) return {};
 
     // Crée une nouvelle matrice avec les dimensions inversées
-    vector<vector<double>> transposed(matrix[0].size(), vector<double>(matrix.size()));
+    vector<vector<long double>> transposed(matrix[0].size(), vector<long double>(matrix.size()));
 
     // Remplissage de la matrice transposée
     for (size_t i = 0; i < matrix.size(); i++) {
@@ -88,7 +88,7 @@ vector<vector<double>> transpose_matrice(const vector<vector<double>>& matrix) {
 
 //******************************************************************************************************************************
 
-vector<vector<double>> produit_matrices(const vector<vector<double>>& mat1, const vector<vector<double>>& mat2) {
+vector<vector<long double>> produit_matrices(const vector<vector<long double>>& mat1, const vector<vector<long double>>& mat2) {
     
     // Vérification de la compatibilité des matrices pour la multiplication
     if (mat1.empty() || mat2.empty() || mat1[0].size() != mat2.size()) {
@@ -96,7 +96,7 @@ vector<vector<double>> produit_matrices(const vector<vector<double>>& mat1, cons
     }
 
     // Initialisation de la matrice résultante avec des zéros
-    vector<vector<double>> result(mat1.size(), vector<double>(mat2[0].size(), 0));
+    vector<vector<long double>> result(mat1.size(), vector<long double>(mat2[0].size(), 0));
 
     // Boucle sur chaque ligne de mat1
     for (size_t i = 0; i < mat1.size(); ++i) {
@@ -114,7 +114,7 @@ vector<vector<double>> produit_matrices(const vector<vector<double>>& mat1, cons
 
 //******************************************************************************************************************************
 
-vector<double> appliquer_matrice_vecteur(const vector<vector<double>>& mat, const vector<double>& vec) {
+vector<long double> appliquer_matrice_vecteur(const vector<vector<long double>>& mat, const vector<long double>& vec) {
     
     // Vérifier si la matrice est vide ou si le nombre de colonnes de la matrice ne correspond pas à la taille du vecteur
     if (mat.empty() || mat[0].size() != vec.size()) {
@@ -122,7 +122,7 @@ vector<double> appliquer_matrice_vecteur(const vector<vector<double>>& mat, cons
     }
 
     // Initialisation du vecteur résultant
-    vector<double> result(mat.size(), 0.0);
+    vector<long double> result(mat.size(), 0.0);
 
     // Boucle sur chaque ligne de la matrice
     for (size_t i = 0; i < mat.size(); i++) {
@@ -138,8 +138,8 @@ vector<double> appliquer_matrice_vecteur(const vector<vector<double>>& mat, cons
 
 //******************************************************************************************************************************
 
-bool isValide(vector<vector<double>> matrice){
-    for (vector<double> e: matrice){
+bool isValide(vector<vector<long double>> matrice){
+    for (vector<long double> e: matrice){
         if(matrice[0].size() != e.size()){
             std::cout<< "matrice invalide"<< std::endl;
             return 0;
@@ -153,16 +153,16 @@ bool isValide(vector<vector<double>> matrice){
 // Cette fonction réalise la décomposition de Cholesky d'une matrice symétrique définie positive
 // Précondition : La matrice en entrée doit etre définie positive sinon la fonction renvoie une erreur.
 
-vector<vector<double>> cholesky_decomposition(const vector<vector<double>>& A) {
+vector<vector<long double>> cholesky_decomposition(const vector<vector<long double>>& A) {
     size_t n = A.size();
-    vector<vector<double>> B(n, vector<double>(n, 0.0));
+    vector<vector<long double>> B(n, vector<long double>(n, 0.0));
 
     for (size_t j = 0; j < n; j++) {
-        double sum = 0.0;
+        long double sum = 0.0;
         for (size_t k = 0; k < j; k++) {
             sum += B[j][k] * B[j][k];
         }
-        double diag = A[j][j] - sum;
+        long double diag = A[j][j] - sum;
         if (diag <= 0) {
             throw std::runtime_error("La matrice n'est pas définie positive.");
         }
@@ -182,9 +182,9 @@ vector<vector<double>> cholesky_decomposition(const vector<vector<double>>& A) {
 
 //******************************************************************************************************************************
 
-vector<double> substitution_avant(const vector<vector<double>>& L, const vector<double>& b) {
+vector<long double> substitution_avant(const vector<vector<long double>>& L, const vector<long double>& b) {
     size_t n = L.size();
-    vector<double> y(n, 0.0);
+    vector<long double> y(n, 0.0);
 
     for (size_t i = 0; i < n; ++i) {
         y[i] = b[i];
@@ -199,9 +199,9 @@ vector<double> substitution_avant(const vector<vector<double>>& L, const vector<
 
 //******************************************************************************************************************************
 
-vector<double> substitution_arriere(const vector<vector<double>>& U, const vector<double>& y) {
+vector<long double> substitution_arriere(const vector<vector<long double>>& U, const vector<long double>& y) {
     size_t n = U.size();
-    vector<double> x(n, 0.0);
+    vector<long double> x(n, 0.0);
 
     for (int i = n - 1; i >= 0; --i) {
         x[i] = y[i];
@@ -217,45 +217,45 @@ vector<double> substitution_arriere(const vector<vector<double>>& U, const vecto
 //******************************************************************************************************************************
 // Function to calculate the solution of the normal equation A^TAx = A^Tb
 
-vector<double> resoudre_equation_normale(const vector<vector<double>>& A, const vector<double>& b) {
+vector<long double> resoudre_equation_normale(const vector<vector<long double>>& A, const vector<long double>& b) {
     
     // Vérifiez que le nombre de lignes de A correspond à la taille de b
     if (A.empty() || A.size() != b.size()) {
         throw invalid_argument("La matrice et le vecteur ne sont pas compatibles pour résoudre l'équation normale.");
     }
     
-    vector<vector<double>> At = transpose_matrice(A); // At est la transposée de A
-    vector<vector<double>> AtA = produit_matrices(At, A); // AtA est le produit de At par A
-    vector<double> Atb = appliquer_matrice_vecteur(At, b); // Atb est le produit de At par b
+    vector<vector<long double>> At = transpose_matrice(A); // At est la transposée de A
+    vector<vector<long double>> AtA = produit_matrices(At, A); // AtA est le produit de At par A
+    vector<long double> Atb = appliquer_matrice_vecteur(At, b); // Atb est le produit de At par b
 
-    vector<vector<double>> B = cholesky_decomposition(AtA); // B est la décomposition de Cholesky de AtA
-    vector<vector<double>> Bt = transpose_matrice(B); // Bt est la transposée de B
+    vector<vector<long double>> B = cholesky_decomposition(AtA); // B est la décomposition de Cholesky de AtA
+    vector<vector<long double>> Bt = transpose_matrice(B); // Bt est la transposée de B
 
     // Résoudre By = Atb pour y par substitution avant
-    vector<double> y = substitution_avant(B, Atb);
+    vector<long double> y = substitution_avant(B, Atb);
     // Résoudre B^Tx = y pour x par substitution arrière
-    vector<double> x_chapeau = substitution_arriere(Bt, y);
+    vector<long double> x_chapeau = substitution_arriere(Bt, y);
 
     return x_chapeau;
 }
 
 //Test :
 // Définissons une matrice A et un vecteur b
-    // vector<vector<double>> A = {
+    // vector<vector<long double>> A = {
     //     {2, -1, 0, 0},
     //     {-1, 2, -1, 0},
     //     {0, -1, 2, -1},
     //     {0, 0, -1, 2}
     // };
-    // vector<double> b = {1, 0, 0, 0};
+    // vector<long double> b = {1, 0, 0, 0};
 
     // // Utilisons la fonction pour calculer la solution des moindres carrés
     // try {
-    //     vector<double> x_chapeau = resoudre_equation_normale(A, b);
+    //     vector<long double> x_chapeau = resoudre_equation_normale(A, b);
         
     //     // Affichons le résultat
     //     cout << "La solution x_chapeau est:" << endl;
-    //     for (double val : x_chapeau) {
+    //     for (long double val : x_chapeau) {
     //         cout << val << " ";
     //     }
     //     cout << endl;
@@ -267,7 +267,7 @@ vector<double> resoudre_equation_normale(const vector<vector<double>>& A, const 
 
 //******************************************************************************************************************************
 
-void readHousingData(const string& filename, vector<vector<double>>& A, vector<double>& b,
+void readHousingData(const string& filename, vector<vector<long double>>& A, vector<long double>& b,
                       bool includeArea = false, bool includeBedrooms = false,
                       bool includeBathrooms = false, bool includeCondoStatus = false,
                       int propertyTypeFilter = -1) {
@@ -292,7 +292,7 @@ void readHousingData(const string& filename, vector<vector<double>>& A, vector<d
         int propertyType = stoi(tokens[4]);  // Index 4 for property type (-1 = aucun, 0 = maison individuelles, 1 = copropriétés)
         if (propertyTypeFilter != -1 && propertyType != propertyTypeFilter) continue;
 
-        vector<double> rowA{1.0};  // Start with the intercept term
+        vector<long double> rowA{1.0};  // Start with the intercept term
 
         if (includeArea) {
             rowA.push_back(stod(tokens[3]));  // Include surface area (index 3)
@@ -319,17 +319,17 @@ void readHousingData(const string& filename, vector<vector<double>>& A, vector<d
 
     //For testing exercice 2.3
     //variables
-    // vector<vector<double>> A;
-    // vector<double> b;
+    // vector<vector<long double>> A;
+    // vector<long double> b;
     // string filename = "housing.data.txt";
 
     // readHousingData(filename, A, b);
 
-    // vector<double> x_chapeau = resoudre_equation_normale(A, b);
+    // vector<long double> x_chapeau = resoudre_equation_normale(A, b);
 
     //     // Affichons le résultat
     //     cout << "La solution x_chapeau est:" << endl;
-    //     for (double val : x_chapeau) {
+    //     for (long double val : x_chapeau) {
     //         cout << val << " ";
     //     }
     //     cout << endl;
@@ -339,13 +339,13 @@ void readHousingData(const string& filename, vector<vector<double>>& A, vector<d
 
 //******************************************************************************************************************************
 
-double calculeSigmaChapeau(const vector<vector<double>>& A, const vector<double>& b, const vector<double>& theta) {
-    vector<double> residus(b.size());
-    double sommeDesCarresDesResidus = 0.0;
+long double calculeSigmaChapeau(const vector<vector<long double>>& A, const vector<long double>& b, const vector<long double>& theta) {
+    vector<long double> residus(b.size());
+    long double sommeDesCarresDesResidus = 0.0;
 
     // Calculer les valeurs prédites
     for (size_t i = 0; i < A.size(); ++i) {
-        double predite = 0.0;
+        long double predite = 0.0;
         for (size_t j = 0; j < A[i].size(); ++j) {
             predite += A[i][j] * theta[j];
         }
@@ -357,13 +357,13 @@ double calculeSigmaChapeau(const vector<vector<double>>& A, const vector<double>
     return std::sqrt(sommeDesCarresDesResidus / b.size());
 }
 
-    // vector<vector<double>> A;
-    // vector<double> b;
+    // vector<vector<long double>> A;
+    // vector<long double> b;
     // string filename = "housing.data.txt"; // Ensure this path is correct
 
     // readHousingData(filename, A, b);
-    // vector<double> theta = resoudre_equation_normale(A, b);
-    // double sigmaHat = calculeSigmaChapeau(A, b, theta);
+    // vector<long double> theta = resoudre_equation_normale(A, b);
+    // long double sigmaHat = calculeSigmaChapeau(A, b, theta);
 
     // cout << "Sigma Chapeau: " << sigmaHat << endl;
 
@@ -371,7 +371,7 @@ double calculeSigmaChapeau(const vector<vector<double>>& A, const vector<double>
 
 //******************************************************************************************************************************
 // For debug purposes
-void writeMatrixToFile(const vector<vector<double>>& matrix, const string& filename) {
+void writeMatrixToFile(const vector<vector<long double>>& matrix, const string& filename) {
     ofstream outFile(filename);  // Create an ofstream to write to the file
     if (!outFile.is_open()) {
         cerr << "Failed to open file: " << filename << endl;
@@ -394,20 +394,20 @@ void writeMatrixToFile(const vector<vector<double>>& matrix, const string& filen
 //******************************************************************************************************************************\
 
 // Function to generate polynomial terms for a vector of variables
-void generatePolynomialTerms(const vector<double>& vars, int degree, vector<double>& row) {
+void generatePolynomialTerms(const vector<long double>& vars, int degree, vector<long double>& row) {
     row.push_back(1.0);  // Add intercept term
     for (int i = 1; i <= degree; ++i) {
-        for (double var : vars) {
+        for (long double var : vars) {
             row.push_back(pow(var, i));  // Add variable raised to the power 'i'
         }
     }
 }
 
 // Main function to read data and create the matrix A and vector b
-void createPolynomialRegressionMatrix(vector<vector<double>>& data, int degree, vector<vector<double>>& A) {
+void createPolynomialRegressionMatrix(vector<vector<long double>>& data, int degree, vector<vector<long double>>& A) {
     for (const auto& row : data) {
-        vector<double> A_row;
-        vector<double> vars(row.begin(), row.end() - 1);  // Extract all variables (assuming last column is the dependent variable)
+        vector<long double> A_row;
+        vector<long double> vars(row.begin(), row.end() - 1);  // Extract all variables (assuming last column is the dependent variable)
         generatePolynomialTerms(vars, degree, A_row);
         A.push_back(A_row);
         //b.push_back(row.back());  // Last element is the dependent variable
@@ -415,7 +415,7 @@ void createPolynomialRegressionMatrix(vector<vector<double>>& data, int degree, 
 }
 
 
-void readpolyData(const string& filename, vector<vector<double>>& A, vector<double>& b) {
+void readpolyData(const string& filename, vector<vector<long double>>& A, vector<long double>& b) {
     ifstream file(filename);
     if (!file.is_open()) {
         throw runtime_error("Could not open file: " + filename);
@@ -425,12 +425,12 @@ void readpolyData(const string& filename, vector<vector<double>>& A, vector<doub
     while (getline(file, line)) {
         stringstream ss(line);
         string token;
-        vector<double> rowA;  // Create a vector for the current row of matrix A
-        double value;  // Variable to store the parsed double value from tokens
+        vector<long double> rowA;  // Create a vector for the current row of matrix A
+        long double value;  // Variable to store the parsed long double value from tokens
 
         // Read each token separated by comma
         while (getline(ss, token, ',')) {
-            // Convert the token to double
+            // Convert the token to long double
             value = stod(token);
             // Add the value to the row of matrix A
             rowA.push_back(value);
@@ -456,17 +456,17 @@ void readpolyData(const string& filename, vector<vector<double>>& A, vector<doub
 
 int main(){
     
-    vector<vector<double>> A;
-    vector<vector<double>> newA;
-    vector<double> b;
-    int polynomialDegree=13;
+    vector<vector<long double>> A;
+    vector<vector<long double>> newA;
+    vector<long double> b;
+    int polynomialDegree=3;
     //readHousingData("housing.data.txt", A, b, true, false, false, false, -1);  
     readpolyData("polynomial.data.txt",A,b);
     
     createPolynomialRegressionMatrix(A, polynomialDegree, newA);
 
-    vector<double> theta = resoudre_equation_normale(newA, b);
-    double sigmaHat = calculeSigmaChapeau(newA, b, theta);
+    vector<long double> theta = resoudre_equation_normale(newA, b);
+    long double sigmaHat = calculeSigmaChapeau(newA, b, theta);
 
     cout <<"pour d= "<<polynomialDegree<< " Sigma Chapeau:  " << sigmaHat << endl;
     
